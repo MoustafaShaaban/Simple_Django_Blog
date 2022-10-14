3813153a9585d2500e21f7ef4a127285caef13b1
Moustafa Shaaban <Moustafa_Shaaban@zohomail.com>
2022-10-14 19:46:23 +0200

    Added user profile functionality
    Users can now visit their profile which lists all the posts that they created

--- blog/urls.py
+++ blog/urls.py
@@ -1,14 +1,16 @@
 from django.urls import path, include
 
-from .views import PostListView, PostDetailView, ContactView, AboutView
+from . import views
 
 
 
 
 urlpatterns = [
-    path('', PostListView.as_view(), name='post-list'),
-    path('posts/<slug:slug>/', PostDetailView.as_view(), name='post-detail'),
-    path('contact/', ContactView.as_view(), name='contact'),
-    path('about/', AboutView.as_view(), name='about'),
+    path('', views.PostListView.as_view(), name='post-list'),
+    path('posts/<slug:slug>/', views.PostDetailView.as_view(), name='post-detail'),
+    path('profile/', views.ProfilePostListView.as_view(), name='profile'),
+    path('create-post/', views.CreatePost.as_view(), name='create-post'),
+    path('contact/', views.ContactView.as_view(), name='contact'),
+    path('about/', views.AboutView.as_view(), name='about'),
     
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
 from django.views.generic.base import TemplateView
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 from .models import Post
 
@@ -11,9 +12,29 @@ class PostListView(ListView):
     paginate_by = 4
 
 
+class ProfilePostListView(ListView):
+    model = Post
+    context_object_name = 'posts'
+    template_name = 'post_list.html'
+    paginate_by = 4
+
+    def get_queryset(self):
+        return Post.objects.filter(author=self.request.user)
+
+
 class PostDetailView(DetailView):
     model = Post
     template_name = 'post_detail.html'
+
+
+class CreatePost(CreateView, LoginRequiredMixin):
+    model = Post
+    fields = ['title', 'content']
+    template_name = 'posts/create_post.html'
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
 
 
 class AboutView(TemplateView):
